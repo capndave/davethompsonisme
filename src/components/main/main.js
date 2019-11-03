8cc0c4e484b734cbfba1ea387733ef2bad925b8d
capndave <capndavet@gmail.com>
2019-11-03 17:27:42 -0600

    Updated file structure

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,16 +1,56 @@
 import React from "react"
-import DaveImage from './daveImage'
-import MainContent from './mainContent'
 import './main.css'
+import Projects from './projects/projects'
+import AboutMe from './aboutMe/aboutMe'
+import '../../assets/icons/css/mfglabs.css'
 
 class Main extends React.Component {
-  render () {
-      return (
-        <main id='main'>
-          {/*<DaveImage /> */}
-          <MainContent />
-        </main>
-      )
+  constructor(props) {
+    super(props)
+    this.state = {
+      activeElementIndex: 0
+    }
+    this.toggleActiveElementIndex = this.toggleActiveElementIndex.bind(this)
+  }
+
+  toggleActiveElementIndex() {
+    this.setState({
+      activeElementIndex: +!this.state.activeElementIndex
+    })
+  }
+
+  render() {
+    const carouselElements = [
+      {
+        name: 'About Me',
+        content: <AboutMe />
+      },
+      {
+        name: 'My Work',
+        content: <Projects />
+      }
+    ]
+
+    let activeElement = carouselElements[this.state.activeElementIndex]
+
+    return (
+      <section id='main'>
+        <div
+          className="icon-left-open"
+          onClick={this.toggleActiveElementIndex}
+        ></div>
+        <article id='main-content'>
+          <h2 className='text-align-center width-100-percent'>
+            {activeElement.name}
+          </h2>
+          {activeElement.content}
+        </article>
+        <div
+          className="icon-right-open"
+          onClick={this.toggleActiveElementIndex}
+        ></div>
+      </section>
+    )
   }
 }
 
---
+++ src/components/main/projects/projects.js
@@ -0,0 +1,64 @@
+import React from "react"
+import './projects.css'
+import Project from './project/project'
+import bluebonnetRecords from '../../../assets/projects/bluebonnetrecords.png'
+import taxpayerSignin from '../../../assets/projects/arbq_signin.png'
+import taxpayerPanel from '../../../assets/projects/arbq_panel.png'
+import lilNonnas from '../../../assets/projects/lilnonnas.png'
+import mollywoodStudio from '../../../assets/projects/mollywoodstudio.png'
+import rareDarey from '../../../assets/projects/raredarey.png'
+
+class Projects extends React.Component {
+  render() {
+    const data = [
+      {
+        name: 'TCAD ARB Apps',
+        builtWith: 'Vue and Vuex. Back end built with Node, Docker, SQL Server and Redis.',
+        description: 'Suite of five applications facilitating customer sign in and management, similar to what they use at the DMV.',
+        image: taxpayerSignin,
+        link: null
+      },
+      {
+        name: 'Bluebonnet Records',
+        builtWith: 'Webpack, HTML, CSS, and JavaScript.',
+        description: 'Commercial website for Bluebonnet Records in Lockhart, TX',
+        image: bluebonnetRecords,
+        link: 'bluebonnet-records.com'
+      },
+      {
+        name: 'Mollywood Studio',
+        builtWith: 'HTML, CSS, and JavaScript.',
+        description: 'Commercial website for Mollywood Studio photography in Los Angeles, CA',
+        image: mollywoodStudio,
+        link: 'mollywoodstudio.com'
+      },
+      {
+        name: 'Lil Nonna\'s',
+        builtWith: 'HTML, CSS, and JavaScript. Uses Google Sheets as a minimal CMS.',
+        description: 'Commercial website for Lil Nonna\'s Pizza in Austin, TX',
+        image: lilNonnas,
+        link: 'lilnonnas.com'
+      },
+      {
+        name: 'Rare Darey',
+        builtWith: 'React. Site in progress!',
+        description: 'Commercial website for Rare Darey, dairy-free cheese maker',
+        image: rareDarey,
+        link: 'raredarey.com'
+      }
+    ]
+    const projects = data.map(d => {
+      return (
+        <Project data={d} />
+      )
+    })
+
+    return (
+      <article id='projects'>
+        {projects}
+      </article>
+    )
+  }
+}
+
+export default Projects
