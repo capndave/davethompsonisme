69408e5165598101155faa794c437a1f961279a2
capndave <capndavet@gmail.com>
2019-11-03 20:00:18 -0600

    Ensured that correct title image shows up for page width

--- src/components/header/titleImage/titleImage.js
+++ src/components/header/titleImage/titleImage.js
@@ -1,28 +1,29 @@
 import React from "react"
-// import DesktopTitle from './svgs/desktopTitle.inline.svg'
-import { ReactComponent as MobileTitle } from '../svgs/mobileTitle.inline.svg'
+import { ReactComponent as DesktopTitle } from '../svgs/desktopTitle.svg'
+import { ReactComponent as MobileTitle } from '../svgs/mobileTitle.svg'
 import initialRender from '../initialRender'
 import './titleImage.css'
 
 class TitleImage extends React.Component {
 
   componentDidMount() {
-
     /* Render svg and animate its groups */
     const svgs = document.getElementsByTagName("svg")
     const groups = svgs[0].querySelectorAll("g")  // get gs from current
     initialRender(groups); //sets off initial title render on gs
-
   }
 
+  
+
   render() {
+    console.log(window.innerWidth)
+    const titleToUse = window.innerWidth > 750 ? <DesktopTitle /> : <MobileTitle />
     return (
-      <div>
-        <MobileTitle />
+      <div className="titleImage">
+        { titleToUse }
       </div>
     )
   }
-        // <DesktopTitle ref={this.desktopTitle} />
 }
 
 export default TitleImage
