abda9539ff85fbe4fc09abbbdc97fc381974f4d7
capndave <capndavet@gmail.com>
2019-11-05 15:38:37 -0600

    Changed structure and style to allow arrow/toggle on root element

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,13 +3,41 @@ import Header from '../header/header'
 import Main from '../main/main'
 import './app.css'
 
-function App() {
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      activeElementIndex: 0
+    }
+    this.toggleActiveElementIndex = this.toggleActiveElementIndex.bind(this)
+  }
+
+  toggleActiveElementIndex() {
+    console.log('Clicked toggleActiveElementIndex')
+    this.setState({
+      activeElementIndex: +!this.state.activeElementIndex
+    })
+  }
+ 
+  render() {
   return (
     <div className="App">
-      <Header />
-      <Main />
+      <div 
+        className="icon-left-open"
+        onClick={this.toggleActiveElementIndex}
+      ></div>
+      <div id="content">
+        <Header />
+        <Main activeElementIndex={this.state.activeElementIndex} />
+      </div>
+      <div
+        className="icon-right-open"
+        onClick={this.toggleActiveElementIndex}
+      ></div>
     </div>
   );
+  }
 }
 
 export default App;
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -4,54 +4,31 @@ import Projects from './projects/projects'
 import AboutMe from './aboutMe/aboutMe'
 import '../../assets/icons/css/mfglabs.css'
 
-class Main extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      activeElementIndex: 0
+const Main = function (props) {
+
+  const carouselElements = [
+    {
+      name: 'About Me',
+      content: <AboutMe />
+    },
+    {
+      name: 'My Work',
+      content: <Projects />
     }
-    this.toggleActiveElementIndex = this.toggleActiveElementIndex.bind(this)
-  }
+  ]
 
-  toggleActiveElementIndex() {
-    this.setState({
-      activeElementIndex: +!this.state.activeElementIndex
-    })
-  }
+  let activeElement = carouselElements[props.activeElementIndex]
 
-  render() {
-    const carouselElements = [
-      {
-        name: 'About Me',
-        content: <AboutMe />
-      },
-      {
-        name: 'My Work',
-        content: <Projects />
-      }
-    ]
-
-    let activeElement = carouselElements[this.state.activeElementIndex]
-
-    return (
-      <section id='main'>
-        <div
-          className="icon-left-open"
-          onClick={this.toggleActiveElementIndex}
-        ></div>
-        <article id='main-content'>
-          <h2 className='text-align-center width-100-percent'>
-            {activeElement.name}
-          </h2>
-          {activeElement.content}
-        </article>
-        <div
-          className="icon-right-open"
-          onClick={this.toggleActiveElementIndex}
-        ></div>
-      </section>
-    )
-  }
+  return (
+    <section id='main'>
+      <article id='main-content'>
+        <h2 className='text-align-center width-100-percent'>
+          {activeElement.name}
+        </h2>
+        {activeElement.content}
+      </article>
+    </section>
+  )
 }
 
 export default Main
