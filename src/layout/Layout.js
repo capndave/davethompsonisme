f49f65749354b227a30445dcc72014b52b877d9c
David Thompson <capndavet@gmail.com>
2021-09-25 16:09:13 -0500

    added style

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Header from '../components/Header/Header'
+import './layout.css'
 
 export default function Layout ({ children }) {
   return (
-		<div>
+		<div id='app'>
 			<Header visible={true} fadeIn={false} />
 			{children}
 		</div>
