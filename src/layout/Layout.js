31dc71f342927494bd0ecb00cf529e425a2fc2a7
David Thompson <capndavet@gmail.com>
2021-10-16 11:56:06 -0500

    add margin to pages

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -4,44 +4,46 @@ import title from '../mixins/animateTitle'
 import cookie from '../mixins/cookie'
 import './layout.css'
 
-export default function Layout ({ children }) {
-  const [fadeIn, setFadein] = useState(true)
-  const [visible, setVisible] = useState(true)
-  const [shouldAnimate, setShouldAnimate] = useState(false)
-  const svgRef = createRef()
+export default function Layout({ children }) {
+	const [fadeIn, setFadein] = useState(true)
+	const [visible, setVisible] = useState(true)
+	const [shouldAnimate, setShouldAnimate] = useState(false)
+	const svgRef = createRef()
 
-  /**
+	/**
 	*  Check if cookie exists and update UI appropriately
 	*  Cookie shows whether logged in in the past day
 	*/
-  function handleAppearance () {
-    if (cookie.exists()) {
-      console.log('cookie exists')
-      // title.appear(svgRef)
-      // makeContentVisible()
-    } else {
-      console.log('cookie does not exist')
-      title.appear(svgRef)
-      // title.animate(svgRef)
-      // .then(this.revealContent.bind(this))
-    }
-    // cookie.set() // Update cookie to show site was visited
-  }
+	function handleAppearance() {
+		if (cookie.exists()) {
+			console.log('cookie exists')
+			// title.appear(svgRef)
+			// makeContentVisible()
+		} else {
+			console.log('cookie does not exist')
+			title.appear(svgRef)
+			// title.animate(svgRef)
+			// .then(this.revealContent.bind(this))
+		}
+		// cookie.set() // Update cookie to show site was visited
+	}
 
-  /**
+	/**
  * Make the title and content appear
  */
-  function makeContentVisible () {
-    title.appear()
-    setVisible(true)
-  }
+	function makeContentVisible() {
+		title.appear()
+		setVisible(true)
+	}
 
-  // handleAppearance()
+	// handleAppearance()
 
-  return (
+	return (
 		<div id='app'>
 			<Header ref={svgRef} shouldAnimate={shouldAnimate} />
-			{children}
+			<main id='page'>
+				{children}
+			</main>
 		</div>
-  )
+	)
 }
