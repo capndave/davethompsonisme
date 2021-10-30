87cfd9fc804435b9c8ec9cb9fd3c9c215870ddc7
David Thompson <capndavet@gmail.com>
2021-10-30 13:31:51 -0500

    initial rss feed setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,62 +1,132 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: 'https://www.davethompsonisme.tld',
-    title: 'davethompsonisme'
-  },
-  plugins: [
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-image',
-    'gatsby-plugin-mdx',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: './src/assets/images/'
-      },
-      __key: 'images'
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'pages',
-        path: './src/pages/'
-      },
-      __key: 'pages'
-    },
-    {
-      resolve: 'gatsby-plugin-prettier-eslint',
-      options: {
-        watch: true,
-        initialScan: false,
-        prettier: {
-          patterns: [
-            // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
-            '**/*.{css,scss,less}',
-            '**/*.{json,json5}',
-            '**/*.{graphql}',
-            '**/*.{md,mdx}',
-            '**/*.{html}',
-            '**/*.{yaml,yml}'
-          ]
-        },
-        eslint: {
-          patterns: '**/*.{js,jsx,ts,tsx}',
-          customOptions: {
-            fix: true,
-            cache: true
+	siteMetadata: {
+		siteUrl: 'https://www.davethompsonisme.tld',
+		title: 'davethompsonisme'
+	},
+	plugins: [
+		'gatsby-plugin-styled-components',
+		'gatsby-plugin-image',
+		'gatsby-plugin-mdx',
+		'gatsby-plugin-sharp',
+		'gatsby-transformer-sharp',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: './src/assets/images/'
+			},
+			__key: 'images'
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'pages',
+				path: './src/pages/'
+			},
+			__key: 'pages'
+		},
+		{
+			resolve: 'gatsby-plugin-prettier-eslint',
+			options: {
+				watch: true,
+				initialScan: false,
+				prettier: {
+					patterns: [
+						// the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
+						'**/*.{css,scss,less}',
+						'**/*.{json,json5}',
+						'**/*.{graphql}',
+						'**/*.{md,mdx}',
+						'**/*.{html}',
+						'**/*.{yaml,yml}'
+					]
+				},
+				eslint: {
+					patterns: '**/*.{js,jsx,ts,tsx}',
+					customOptions: {
+						fix: true,
+						cache: true
+					}
+				}
+			}
+		},
+		{
+			resolve: 'gatsby-plugin-react-svg',
+			options: {
+				rule: {
+					include: /titles/
+				}
+			}
+		},
+		{
+			resolve: `gatsby-plugin-canonical-urls`,
+			options: {
+				// Change `siteUrl` to your domain 
+				siteUrl: `https://davethompsonisme.com`,
+
+				// Query string parameters are inclued by default.
+				// Set `stripQueryString: true` if you don't want `/blog` 
+				// and `/blog?tag=foobar` to be indexed separately
+				stripQueryString: true
+			}
+		},
+		{
+			resolve: `gatsby-plugin-feed`,
+			options: {
+				query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
           }
         }
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-react-svg',
-      options: {
-        rule: {
-          include: /titles/
-        }
-      }
-    }
-  ]
+      `,
+				feeds: [
+					{
+						serialize: ({ query: { site, allMarkdownRemark } }) => {
+							return allMarkdownRemark.edges.map(edge => {
+								return Object.assign({}, edge.node.frontmatter, {
+									description: edge.node.excerpt,
+									date: edge.node.frontmatter.date,
+									url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+									guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+									custom_elements: [{ "content:encoded": edge.node.html }],
+								})
+							})
+						},
+						query: `
+            {
+              allMarkdownRemark(
+                sort: { order: DESC, fields: [frontmatter___date] },
+              ) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    fields { slug }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            }
+          `,
+						output: "/rss.xml",
+						title: "Your Site's RSS Feed",
+						// optional configuration to insert feed reference in pages:
+						// if `string` is used, it will be used to create RegExp and then test if pathname
+						// of current page satisfied this regular expression;
+						// if not provided or `undefined`, all pages will have feed reference inserted
+						match: "^/blog/",
+					},
+				],
+			}
+		}
+	]
 }
