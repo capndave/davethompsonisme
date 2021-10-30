cc24c6071ad499220d7d6734fdc68f0214920246
David Thompson <capndavet@gmail.com>
2021-10-30 10:15:43 -0500

    finish about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import './about.css'
 
 export default function About () {
@@ -7,7 +8,7 @@ export default function About () {
 			<p>I build websites and apps for small businesses, and am a Software Engineer at <a href="https://www.travelers.com/">Travelers Insurance</a>.</p>
 			<p>Before becoming a developer I played music, did social work, statistical computing, and made pretty graphs as a data visualization specialist.</p>
 			<p>I'm good at Node.js, UI and graphic design, and assorted program-y things.</p>
-			<p>Click right to see my work.</p>
+			<p>Click <Link to='/projects/'>here</Link> to see my work.</p>
 		</article>
   )
 }
