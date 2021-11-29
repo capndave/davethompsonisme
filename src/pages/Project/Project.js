9d842aeebcabd770f04396df95fc93adf6d2177c
David Thompson <capndavet@gmail.com>
2021-11-28 18:41:50 -0600

    clean up Project.js code

--- src/pages/Project/Project.js
+++ src/pages/Project/Project.js
@@ -25,7 +25,7 @@ function Project(props) {
 	return (
 		<section
 			// className={`project ${isHovered ? 'hovered' : null}`} // id conditional on hover
-			className='project'// id conditional on hover
+			className='project'
 			onMouseEnter={() => setIsHovered(true)}
 			onMouseLeave={() => setIsHovered(false)}
 		>
@@ -35,63 +35,3 @@ function Project(props) {
 }
 
 export default Project
-
-// export default class Project extends React.Component {
-// 	constructor() {
-// 		super()
-// 		this.state = {
-// 			isFlipped: false
-// 		}
-// 		this.handleClick = this.handleClick.bind(this)
-// 		this.handleHover = this.handleHover.bind(this)
-// 	}
-
-// 	handleHover(e) {
-// 		e.preventDefault()
-// 		this.setState(prevState => ({ isFlipped: !prevState.isFlipped }))
-// 	}
-
-// 	handleClick(e) {
-// 		e.preventDefault()
-// 		this.setState(prevState => ({ isFlipped: !prevState.isFlipped }))
-// 	}
-
-// 	render() {
-// 		const link =
-// 			this.props.data.link ? <a href={this.props.data.link}>{this.props.data.link}</a> : null
-
-// 		return (
-// 			<div
-// 				isFlipped={this.state.isFlipped}
-// 				flipDirection='horizontal'
-// 				cardZIndex='0'
-// 			>
-// 				<div
-// 					className='project front'
-// 					key='front'
-// 					onClick={this.handleClick}
-// 					onMouseEnter={this.handleHover}
-// 				>
-// 					<img
-// 						alt='project'
-// 						className='image'
-// 						src={this.props.data.image}>
-// 					</img>
-// 				</div>
-// 				<div
-// 					className='project back'
-// 					key='back'
-// 					onClick={this.handleClick}
-// 					onMouseLeave={this.handleHover}
-// 				>
-// 					<div className='text'>
-// 						<h3>{this.props.data.name}</h3>
-// 						<p>{this.props.data.description}</p>
-// 						<p>Made with {this.props.data.builtWith}</p>
-// 						<p>{link}</p>
-// 					</div>
-// 				</div>
-// 			</div>
-// 		)
-// 	}
-// }
