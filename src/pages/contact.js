c77c5114d33b7dbb0dd6604c06bb245a4783f1e0
David Thompson <capndavet@gmail.com>
2021-12-05 11:01:21 -0600

    fixed imports on contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 export default function Contact () {
   return (
