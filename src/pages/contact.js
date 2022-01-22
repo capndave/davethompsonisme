3bda528fc05fabeb9cc4f411d52afb3237c90e35
David Thompson <capndavet@gmail.com>
2022-01-22 09:53:52 -0600

    move Icons module to components

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ContactForm from '../components/ContactForm/ContactForm'
-import Icons from './Icons/Icons'
+import Icons from '../components/Icons/Icons'
 import useWindowSize from '../hooks/useWindowSize'
 
 export default function Contact () {
