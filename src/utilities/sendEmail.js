71f8ea3e4e16c6a0d0466cfc8a211004c5ed1109
David Thompson <capndavet@gmail.com>
2022-02-27 11:04:02 -0600

    clean up sendEmail.js

--- src/utilities/sendEmail.js
+++ src/utilities/sendEmail.js
@@ -1,10 +1,12 @@
 import emailjs from '@emailjs/browser'
 
 require('dotenv').config({
-  path: process.env.PUBLIC_URL
-    ? `${process.env.PUBLIC_URL}/.env.${process.env.NODE_ENV}`
-    : `.env.${process.env.NODE_ENV}`
+	path: process.env.PUBLIC_URL
+		? `${process.env.PUBLIC_URL}/.env.${process.env.NODE_ENV}`
+		: `.env.${process.env.NODE_ENV}`
 })
+
+const { EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, EMAILJS_USER_ID } = process.env
 
 /**
  * EmailJS is a service used to send mail from client-side code
@@ -13,10 +15,9 @@ require('dotenv').config({
  * It can be configured in the EmailJS Interface
  */
 
-const { EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, EMAILJS_USER_ID } = process.env
 emailjs.init(EMAILJS_USER_ID)
 
 export default function ({ subject, name, message, email }) {
-  return emailjs.send(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, { subject, name, message, email })
-    .catch(err => console.error('Error sending email:', err))
+	return emailjs.send(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, { subject, name, message, email })
+		.catch(err => console.error('Error sending email:', err))
 }
--- src/utilities/sendEmail.test.js
+++ src/utilities/sendEmail.test.js
@@ -1,27 +1,14 @@
 import sendEmail from './sendEmail'
 
 const emailDetails = {
-  subject: 'test subject',
-  name: 'Test Name',
-  message: 'Hi from a the test',
-  email: 'test@email.com'
+	subject: 'test subject',
+	name: 'Test Name',
+	message: 'Hi from a the test',
+	email: 'test@email.com'
 }
 
 it('sends an email', async () => {
-  const response = await sendEmail(emailDetails)
-  expect(response.status).toBe(200)
-  expect(response.text).toBe('OK')
-})
-
-// TODO
-it.skip('fails if no argument is passed', async () => {
-  let results = false
-  try {
-    results = await sendEmail()
-  } catch (err) {
-    console.log(err)
-  }
-
-  console.log(results)
-  expect(results.length).toBe(0)
+	const response = await sendEmail(emailDetails)
+	expect(response.status).toBe(200)
+	expect(response.text).toBe('OK')
 })
