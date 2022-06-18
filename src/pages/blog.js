e84d6543305b1c49af241bc6d7f9bc8c7531d3da
David Thompson <capndavet@gmail.com>
2022-06-18 09:34:36 -0500

    show links to posts on blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,37 @@
+import { graphql, Link } from 'gatsby'
 import React from 'react'
 import './blog.css'
 
-function Blog () {
+function Blog ({ data }) {
   return (
-		<article id='blog'>
-			blogs
+		<article>
+			{data.allMdx.nodes.map(({ id, excerpt, frontmatter, slug }) => (
+				<div>
+					<Link to={`/${slug}`}>
+						<h3>{frontmatter.title}</h3>
+						<p>{frontmatter.date}</p>
+						<p>{excerpt}</p>
+					</Link>
+				</div>
+			))}
 		</article>
   )
 }
 
 export default Blog
+
+export const query = graphql`
+					query SITE_INDEX_QUERY {
+						allMdx(sort: {fields: [frontmatter___date], order: DESC }) {
+						nodes {
+						id
+        excerpt(pruneLength: 250)
+					frontmatter {
+						title
+          date(formatString: "YYYY MMMM Do")
+        }
+					slug
+      }
+    }
+  }
+					`
